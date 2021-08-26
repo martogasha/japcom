19fd4a412d02449b77dea10f2c341939cd188f8b
Your Name <you@example.com>
2021-08-26 18:02:31 +0300

    kjshdfkeergergghrergrgrgg

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -50,7 +50,7 @@ class MpesaController extends Controller
 
         $json_str = file_get_contents('https://jnl.co.ke/api/storeWebhooks');
 
-        $response = $webhooks->webhookHandler($json_str, $_SERVER['HTTP_X_KOPOKOPO_SIGNATURE']);
+        $response = $webhooks->webhookHandler($json_str, $_SERVER['vaoQShrNB_sJvdNWZVliGzc1_RFzmX8dtMEbkl4ETds']);
         $store = Mpesa::create([
             'idno'=>$request->$response['status'],
             'topic'=>$response['status'],
