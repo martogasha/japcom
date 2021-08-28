02475c796d73eaf2e94657c7e775b882c630503a
Your Name <you@example.com>
2021-08-28 20:19:45 +0300

    jsdffjdfwefw

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -42,7 +42,7 @@ class MpesaController extends Controller
         dd($response);
     }
 
-    public function storeWebhooks(Request $request){
+    public function storeWebhooks(){
         $options = [
             'clientId' => 'Y4oqKYiZbuy5jH3yTojM6sdi0MLlmey_Rkrx6bpOj1g',
             'clientSecret' => 'eeF7KX3QE9bmOWnEI4FY6zfskzsbaYp9hiMZIXRz6QY',
@@ -55,9 +55,7 @@ class MpesaController extends Controller
         $webhooks = $K2->Webhooks();
 
         $json_str = file_get_contents('https://jnl.co.ke/api/storeWebhooks');
-        $store = Mpesa::create([
-            'data'=>$request->$json_str
-        ]);
+    var_dump($json_str);
 
         $response = $webhooks->webhookHandler($json_str, $_SERVER['HTTP_X_KOPOKOPO_SIGNATURE']);
         $data =  json_encode($response);
