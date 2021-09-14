535537a4d09dea60d4d47019c08bfffc8c41df78
Your Name <you@example.com>
2021-09-14 13:59:00 +0300

    akcjsdfsdfsd

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -94,9 +94,9 @@ class MpesaController extends Controller
                         ]);
                         $updateBalance = Invoice::where('user_id',$createPay->user_id)->where('status',0)->update(['balance'=>$currentBalance]);
                         $updateIBalance = Payment::where('invoice_id',$createPay->invoice_id)->where('id',$createPay->id)->update(['invoice_balance'=>$currentBalance]);
-                        $updateCashAmount = Invoice::where('id',$createPay->invoice_id)->update(['mpesa_id'=>$createPayment->id]);
-                        $updateInvoicePayment = Invoice::where('id',$createPay->invoice_id)->update(['payment_id',$createPay->id]);
-                        $updateInvoiceMpesaAmount = Invoice::where('id',$createPay->invoice_id)->update(['mpesa_amount',$createPayment->amount]);
+                        $updateCashAmount = Invoice::where('user_id',$createPay->user_id)->where('status',0)->update(['mpesa_id'=>$createPayment->id]);
+                        $updateInvoicePayment = Invoice::where('user_id',$createPay->user_id)->where('status',0)->update(['payment_id',$createPay->id]);
+                        $updateInvoiceMpesaAmount = Invoice::where('user_id',$createPay->user_id)->where('status',0)->update(['mpesa_amount',$createPayment->amount]);
                         $updateUserAmount = User::where('id',$createPay->user_id)->update(['amount'=>$createPayment->amount]);
                         $updateUserDate = User::where('id',$createPay->user_id)->update(['payment_date'=>$createPayment->date]);
                         $updateUserBalance = User::where('id',$createPay->user_id)->update(['balance'=>$currentBalance]);
