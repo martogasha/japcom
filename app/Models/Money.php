e5c0ed63f7a939109e70e34055d2921b4b173da9
Your Name <you@example.com>
2021-09-23 18:40:48 +0300

    fgsggergrasgr

--- app/Models/Money.php
+++ app/Models/Money.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Money extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'ido',
+        'topic',
+        'created_at',
+        'eventType',
+        'resourceId',
+        'status',
+        'reference',
+        'originationTime',
+        'senderPhoneNumber',
+        'amount',
+        'currency',
+        'tillNumber',
+        'system',
+        'senderFirstName',
+        'senderMiddleName',
+        'senderLastName',
+        'linkSelf',
+        'linkResource',
+        'invoice_id',
+        'invoice_balance',
+    ];
 }
