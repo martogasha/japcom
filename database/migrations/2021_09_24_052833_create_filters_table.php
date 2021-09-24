9414880e01ac0f38c1fd485a581f7bcb72cbd31f
Your Name <you@example.com>
2021-09-24 08:50:06 +0300

    fdfhdfhdstg

--- database/migrations/2021_09_24_052833_create_filters_table.php
+++ database/migrations/2021_09_24_052833_create_filters_table.php
@@ -15,6 +15,26 @@ class CreateFiltersTable extends Migration
     {
         Schema::create('filters', function (Blueprint $table) {
             $table->id();
+            $table->string('ido')->nullable();
+            $table->string('topic')->nullable();
+            $table->string('createdAt')->nullable();
+            $table->string('eventType')->nullable();
+            $table->string('resourceId')->nullable();
+            $table->string('status')->nullable();
+            $table->string('reference')->nullable();
+            $table->string('originationTime')->nullable();
+            $table->string('senderPhoneNumber')->nullable();
+            $table->integer('amount')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('tillNumber')->nullable();
+            $table->string('system')->nullable();
+            $table->string('senderFirstName')->nullable();
+            $table->string('senderMiddleName')->nullable();
+            $table->string('senderLastName')->nullable();
+            $table->string('linkSelf')->nullable();
+            $table->string('linkResource')->nullable();
+            $table->integer('invoice_id')->nullable();
+            $table->integer('invoice_balance')->nullable();
             $table->timestamps();
         });
     }
