b4a302fb2bb9a9a4da0aaf270e5932e48609a5b4
martogasha <m.gachanga97@gmail.com>
2021-11-12 17:37:16 +0300

    sdgfhdfjkdfj

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -139,33 +139,7 @@
                                     </div>
                                     <div class="dropdown">
                                         <div class="row">
-                                            <div class="form-group">
-                                                <label for="sel1">Year</label>
-                                                <select class="form-control" id="year">
-                                                    <option value="2021">2021</option>
-                                                    <option value="2022">2022</option>
-                                                    <option value="2023">2023</option>
-                                                    <option value="2024">2024</option>
-                                                    <option value="2025">2025</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="sel1">Month</label>
-                                                <select class="form-control" id="month">
-                                                    <option value="1">Jan</option>
-                                                    <option value="2">Feb</option>
-                                                    <option value="3">Mar</option>
-                                                    <option value="4">Apr</option>
-                                                    <option value="5">May</option>
-                                                    <option value="6">Jun</option>
-                                                    <option value="7">Jul</option>
-                                                    <option value="8">Aug</option>
-                                                    <option value="9">Sept</option>
-                                                    <option value="10">Oct</option>
-                                                    <option value="11">Nov</option>
-                                                    <option value="12">Dec</option>
-                                                </select>
-                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
