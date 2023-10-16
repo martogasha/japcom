ed59d7f1ff513708ea8254eb1ced602fa39557d7
max <m.gachanga97@gmail.com>
2023-10-16 15:56:07 +0300

    first commitlsdfjklfjs

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -53,6 +53,24 @@ class MpesaController extends Controller
     }
     public function storeWebhooks(Request $request)
     {
+        header("Content-Type: application/json");
+        $stkCallbackResponse = file_get_contents('php://input');
+        $logFile = "Mpesastkresponse.json";
+        $log = fopen($logFile, "a");
+        fwrite($log, $stkCallbackResponse);
+        fclose($log);
+
+        $data = json_decode($stkCallbackResponse);
+
+        $MerchantRequestID = $data->Body->stkCallback->MerchantRequestID;
+        $CheckoutRequestID = $data->Body->stkCallback->CheckoutRequestID;
+        $ResultCode = $data->Body->stkCallback->ResultCode;
+        $ResultDesc = $data->Body->stkCallback->ResultDesc;
+        $Amount = $data->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+        $TransactionId = $data->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+        $UserPhoneNumber = $data->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+        Log::info($Amount);
+
         $duplicate = $request->json()->all();
         Log::info($duplicate);
         $dub = array($duplicate);
