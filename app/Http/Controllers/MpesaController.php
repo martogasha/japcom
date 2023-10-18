45b1d2c2b1f45922bf57946eff2db740b1975ca8
max <m.gachanga97@gmail.com>
2023-10-18 07:34:47 +0300

    first commitlsdfjklfdcadcager

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -37,7 +37,7 @@ class MpesaController extends Controller
         $consumerKey = "HZKs4kTilx4xoc8CGKgR8t3Jkxe6A5Yp"; //Fill with your app Consumer Key
         $consumerSecret = "R2xDmkzkVtBAeU4C"; //Fill with your app Consumer Secret
 //ACCESS TOKEN URL
-        $access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate';
+        $access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
         $headers = ['Content-Type:application/json; charset=utf8'];
         $curl = curl_init($access_token_url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
